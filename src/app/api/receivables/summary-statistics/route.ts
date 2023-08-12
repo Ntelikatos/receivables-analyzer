908bef23b6615c1db7106c581e2fe07be240829a
alexandros <alexandros@appyventures.com>
2023-08-12 12:13:08 +0300

    feat: add extra results in get receivables summary statistics endpoint

--- src/app/api/receivables/summary-statistics/route.ts
+++ src/app/api/receivables/summary-statistics/route.ts
@@ -13,10 +13,34 @@ export async function GET(req: NextRequest) {
 
         const totalClosedInvoices = totalInvoices - totalOpenInvoices;
 
+        const openInvoicesAggregate = await prisma.receivable.aggregate({
+            _sum: {openingValue: true},
+            _avg: {openingValue: true},
+            _max: {openingValue: true},
+            _min: {openingValue: true},
+            where: {closedDate: null}
+        });
+
+        const closedInvoicesAggregate = await prisma.receivable.aggregate({
+            _sum: {paidValue: true},
+            _avg: {paidValue: true},
+            _max: {paidValue: true},
+            _min: {paidValue: true},
+            where: {closedDate: {not: null}}
+        });
+
         const summaryStatistics = {
             totalInvoices,
             totalOpenInvoices,
             totalClosedInvoices,
+            openInvoicesValue: openInvoicesAggregate._sum.openingValue,
+            closedInvoicesValue: closedInvoicesAggregate._sum.paidValue,
+            averageOpenInvoiceValue: openInvoicesAggregate._avg.openingValue,
+            averageClosedInvoiceValue: closedInvoicesAggregate._avg.paidValue,
+            largestOpenInvoice: openInvoicesAggregate._max.openingValue,
+            smallestOpenInvoice: openInvoicesAggregate._min.openingValue,
+            largestClosedInvoice: closedInvoicesAggregate._max.paidValue,
+            smallestClosedInvoice: closedInvoicesAggregate._min.paidValue
         }
 
         return NextResponse.json(summaryStatistics, {headers: {"Content-Type": "application/json"}})
