249444ead48bbb8f2500de666a059d8967eef2dc
alexandros <alexandros@appyventures.com>
2023-08-12 13:07:05 +0300

    feat: add temp test for get receivables summary statistics query in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 'use client'
 
-import {useCreateReceivablesMutation, useGetReceivablesQuery} from "@/redux/services/receivablesApi";
+import {
+    useCreateReceivablesMutation,
+    useGetReceivablesQuery,
+    useGetReceivablesSummaryStatisticsQuery
+} from "@/redux/services/receivablesApi";
 import {Receivable} from "@/app/api/@types";
 import {v4 as uuidv4} from 'uuid';
 
 export default function Home() {
     const {data: receivables, isLoading: isLoadingReceivables} = useGetReceivablesQuery()
+    const {data: summaryStatistics, isLoading: isLoadingSummaryStatistics} = useGetReceivablesSummaryStatisticsQuery()
     const [createReceivables, {isLoading: isLoadingCreateReceivables}] = useCreateReceivablesMutation()
 
     const receivablesDummyPayload: Receivable[] = [
@@ -38,6 +43,8 @@ export default function Home() {
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <h1>Receivables</h1>
             <p>{isLoadingReceivables ? 'Loading...' : JSON.stringify(receivables)}</p>
+            <h1>Receivables Summary Statistics</h1>
+            <p>{isLoadingSummaryStatistics ? 'Loading...' : JSON.stringify(summaryStatistics)}</p>
             <button onClick={() => createReceivables(receivablesDummyPayload)}>Create Receivables</button>
         </main>
     )
