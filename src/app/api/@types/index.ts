81403c4d82b68203f00a9cc1305a178c2c37d641
alexandros <alexandros@appyventures.com>
2023-08-12 12:18:12 +0300

    feat: add ReceivableSummaryStatisticsResponse type

--- src/app/api/@types/index.ts
+++ src/app/api/@types/index.ts
@@ -3,6 +3,20 @@ import {ReceivableSchema} from "@/lib/api/validators/models/receivable";
 
 export type Receivable = z.infer<typeof ReceivableSchema> & { id?: number }
 
+export type ReceivableSummaryStatisticsResponse = {
+    totalInvoices?: number
+    totalOpenInvoices?: number
+    totalClosedInvoices?: number
+    openInvoicesValue?: number,
+    closedInvoicesValue?: number,
+    averageOpenInvoiceValue?: number,
+    averageClosedInvoiceValue?: number,
+    largestOpenInvoice?: number,
+    smallestOpenInvoice?: number,
+    largestClosedInvoice?: number,
+    smallestClosedInvoice?: number
+}
+
 export type CommonResponse = { message: string };
 export type ErrorResponse = { status: string, message: string };
 
