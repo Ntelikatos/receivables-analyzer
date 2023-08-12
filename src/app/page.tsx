c2d8b03b02f087967882f3405f56fe8ce27d9d59
alexandros <alexandros@appyventures.com>
2023-08-12 12:58:00 +0300

    feat: add temp test for create receivables mutation in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,44 @@
 'use client'
 
-import {useGetReceivablesQuery} from "@/redux/services/receivablesApi";
+import {useCreateReceivablesMutation, useGetReceivablesQuery} from "@/redux/services/receivablesApi";
+import {Receivable} from "@/app/api/@types";
+import {v4 as uuidv4} from 'uuid';
 
 export default function Home() {
     const {data: receivables, isLoading: isLoadingReceivables} = useGetReceivablesQuery()
+    const [createReceivables, {isLoading: isLoadingCreateReceivables}] = useCreateReceivablesMutation()
+
+    const receivablesDummyPayload: Receivable[] = [
+        {
+            "reference": "asdasd",
+            "currencyCode": "USD",
+            "issueDate": "2023-02-22",
+            "openingValue": 250,
+            "paidValue": 100,
+            "dueDate": "2023-03-05",
+            "debtorName": "John Doe",
+            "debtorReference": "asdasdad",
+            "debtorCountryCode": "gr"
+        },
+        {
+            "reference": uuidv4(),
+            "currencyCode": "USD",
+            "issueDate": "2023-02-22",
+            "openingValue": 250,
+            "paidValue": 100,
+            "dueDate": "2023-03-05",
+            "closedDate": "2023-03-06",
+            "debtorName": "John Doe",
+            "debtorReference": uuidv4(),
+            "debtorCountryCode": "gr"
+        }
+    ]
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <h1>Receivables</h1>
             <p>{isLoadingReceivables ? 'Loading...' : JSON.stringify(receivables)}</p>
+            <button onClick={() => createReceivables(receivablesDummyPayload)}>Create Receivables</button>
         </main>
     )
 }
